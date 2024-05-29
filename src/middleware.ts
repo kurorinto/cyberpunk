75f4655213b9276b52a4866764e4fc0f58543f33
kurorinto <394460456@qq.com>
2024-05-29 14:09:37 +0800

    feat: 代码优化

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET } from "./request"
 
 // This function can be marked `async` if using `await` inside
 export const middleware = async (request: NextRequest) => {
+  // 获取页面的cookie
   const cookies = request.headers.get('cookie')?.split(';').map(item => item.trim()).reduce((res, item, index) => {
     const splitIndex = item.indexOf('=')
     if (splitIndex !== -1) {
@@ -15,21 +16,22 @@ export const middleware = async (request: NextRequest) => {
     return res
   }, {} as any)
 
+  // 获取accessToken
   const at = cookies?.[ACCESS_TOKEN_KEY]
 
   if (!at) {
+    // 没有accessToken 未登录处理
     return nextFailure({ message: '未登录', code: 401 }, { headers: { 'set-cookie': `${ACCESS_TOKEN_KEY}=;PATH=/;MAX-AGE=0` } })
   }
 
   try {
     const res = await jose.jwtVerify(at, ACCESS_TOKEN_SECRET)
-    console.log(res)
+    // 校验成功
   } catch (error) {
-    console.log('error', error)
+    // 有accessToken 但校验未通过 登录失效处理
     return nextFailure({ message: '登录已失效', code: 402 }, { headers: { 'set-cookie': `${ACCESS_TOKEN_KEY}=;PATH=/;MAX-AGE=0` } })
   }
 
-  console.log('success')
   return next()
 }
 
