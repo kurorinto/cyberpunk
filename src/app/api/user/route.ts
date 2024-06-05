748e2096b1b1b8a1f0f98056651ae2b07a428589
kurorinto <394460456@qq.com>
2024-06-05 17:18:18 +0800

    feat: 修改配置

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,17 +1,6 @@
 import { success } from "@/apiHandlers/resultUtils"
 import pool from "@/db"
 
-export const POST = async (request: Request) => {
-  const body = await request.json()
-  const { name, avatar, username, password } = body
-
-  await pool.execute(
-    `INSERT INTO user (name, avatar, username, password) VALUES ('${name}', '${avatar}', '${username}', '${password}')`,
-  )
-
-  return success(true)
-}
-
 export const GET = async () => {
   const [result] = await pool.query("SELECT * FROM user")
   return success(result)
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,15 @@
+import {
+  DB_HOST, DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} from "@/constants/config"
 import mysql from "mysql2/promise"
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
 })
 
 export default pool
